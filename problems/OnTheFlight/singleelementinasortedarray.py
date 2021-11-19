6156a56edb22dd4e97721616e52dd88333c406cb
Utkarsh Singh <utkarsh.singh0609@gmail.com>
2021-11-19 14:50:05 -0600

    did something on the flight.

--- problems/OnTheFlight/singleelementinasortedarray.py
+++ problems/OnTheFlight/singleelementinasortedarray.py
@@ -8,3 +8,15 @@ Example 2:
 Input: [3,3,7,7,10,11,11]
 Output: 10
 '''
+
+class Solution:
+    def __init__(self, array):
+        self.array  =   array
+        # print(self.array)
+
+    def findTheElement(self):
+        pass
+
+obj1 = Solution([1,1,2,3,3,4,4,8,8])
+# obj1.findTheElement()
+obj2 = Solution([3,3,7,7,10,11,11])
